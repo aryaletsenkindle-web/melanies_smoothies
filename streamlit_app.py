a5ee294550c818275941c1a3ca35fbdec254d7fd
aryaletsenkindle-web <aryaletsenkindle@gmail.com>
2026-01-01 18:19:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 st.dataframe(my_dataframe, use_container_width=True)
 
+ingredients_string = ""  # put this BEFORE the multiselect block
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    options=my_dataframe,
+    options=[row["FRUIT_NAME"] for row in my_dataframe.collect()],
     max_selections=5
 )
 
@@ -27,12 +29,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
-# Insert query only runs if user entered a name and selected ingredients
+my_insert_stmt = f"""
+INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+VALUES ('{ingredients_string.strip()}', '{name_on_order}')
+"""
+
 if name_on_order and ingredients_string:
-    my_insert_stmt = f"""
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES ('{ingredients_string.strip()}', '{name_on_order}')
-    """
-
-
+    session.sql(my_insert_stmt).collect()
+    st.success("Order saved!")
 
