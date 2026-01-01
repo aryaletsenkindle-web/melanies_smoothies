1ee89fe4012cf6e2952f6cb6ed0cfb095733f64e
aryaletsenkindle-web <aryaletsenkindle@gmail.com>
2026-01-01 13:54:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,8 @@ if ingredients and st.button("Submit Order"):
 
     st.success("Your Smoothie is ordered!", icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response
+
 
